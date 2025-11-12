80a7bfb15881dabf8834beb38f3293ca83ab102a
Joaquin Balido Omaña <125173675+joaquinomana@users.noreply.github.com>
2025-11-12 19:59:54 +0800

    Refactor vite.config.js for improved readability

--- vite.config.js
+++ vite.config.js
@@ -3,27 +3,32 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/Portfolio-2025/',
-    plugins: [react()],
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        // Split the swiper plugin library into a separate chunk to avoid a large chunk size on index.js
-                        if (id.includes('swiper'))
-                            return 'swiper';
-                        return;
-                    }
-                }
-            }
-        }
-    },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                silenceDeprecations: ["mixed-decls", "color-functions", "global-builtin", "import"],
-            },
+  base: '/Portfolio-2025/',
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('swiper')) return 'swiper';
+            return;
+          }
         },
+      },
     },
-})
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: [
+          "mixed-decls",
+          "color-functions",
+          "global-builtin",
+          "import",
+        ],
+      },
+    },
+  },
+});
+
+
